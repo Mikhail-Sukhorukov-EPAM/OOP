bd56601fdc6d4e2f8690be1af90c3ad885442c06
Mikhail Sukhorukov <Mikhail_Sukhorukov@epam.com>
2021-04-15 11:01:10 +0300

    lesson 3.1 - the first task

--- 3_Magical_methods/lesson1.py
+++ 3_Magical_methods/lesson1.py
@@ -25,3 +25,42 @@ class User:
         # str(u1)
         # 'It is a user: Mikhail'
         return f"User: {self.user}, List of Users: {User.users}"
+
+
+class Person:
+    def __init__(self, name, surname, gender="male"):
+        self.name = name
+        self.surname = surname
+        self.my_gender = gender
+
+    def __str__(self):
+        if self.gender == "male":
+            return f"Гражданин {self.surname} {self.name}"
+        else:
+            return f"Гражданка {self.surname} {self.name}"
+
+    @property
+    def my_gender(self):
+        return self.gender
+
+    @my_gender.setter
+    def my_gender(self, new_gender):
+        if new_gender == "male":
+            self.gender = new_gender
+        elif new_gender == "female":
+            self.gender = new_gender
+        else:
+            print("Не знаю, что вы имели ввиду? Пусть это будет мальчик!")
+            self.gender = "male"
+
+
+p1 = Person("Mikhail", "Sukhorukov", "male")
+print(p1)
+p2 = Person("Darya", "Motorina", "female")
+print(p2)
+p3 = Person("Darya", "Motorina", "fmale")
+print(p3)
+p1 = Person('Chuck', 'Norris')
+print(p1)  # печатает "Гражданин Norris Chuck"
+p2 = Person('Mila', 'Kunis', 'female')
+print(p2)  # печатает "Гражданка Kunis Mila"
